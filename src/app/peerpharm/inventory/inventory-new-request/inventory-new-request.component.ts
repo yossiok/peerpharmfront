67e914148ef7fa5fb68b31d41b1ead6888ff6e34
USER <gabi@gmail.com>
2018-12-13 17:29:40 +0200

    inventory request

--- src/app/peerpharm/inventory/inventory-new-request/inventory-new-request.component.ts
+++ src/app/peerpharm/inventory/inventory-new-request/inventory-new-request.component.ts
@@ -12,7 +12,11 @@ import * as moment from 'moment';
 })
 export class InventoryNewRequestComponent implements OnInit {
   inventoryReqForm: FormGroup;
+  itemLine: FormGroup;
   reqList:Array<any>=[];// to show added items to request
+  itemNumInput:String='';
+  itemAmount:Number;
+  relatedOrder:String='';
   today:Date=new Date;
 
   invReq={
@@ -34,7 +38,13 @@ export class InventoryNewRequestComponent implements OnInit {
       toWH: ['', Validators.required],
       currDate: [Date, Validators.required],
       deliveryDate: [Date, Validators.required],
-      // reqList: [null, Validators.required],
+      reqList: [Array],
+    });
+    this.itemLine = fb.group({
+      //   'description' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      itemNumInput: ['', Validators.required],
+      itemAmount: [Number, Validators.required],
+      relatedOrder: [''],
     });
 
   }
@@ -61,17 +71,44 @@ export class InventoryNewRequestComponent implements OnInit {
       toWH: form.toWH,
       currDate: form.currDate,   
       deliveryDate: form.deliveryDate,   
-      reqList:[],// itemId,requiredAmount ,suppliedAmount, relatedOrderNum, itemStatus, itemType
+      reqList:this.reqList,
       itemsType: 'components', 
       reqStatus:'open',
     }
-    // this.invReq.itemsType='component'; 
-    // this.invReq.reqStatus='open';
+
     console.log(this.invReq);
     debugger;
     this.inventoryReqService.addNewRequest(this.invReq).subscribe(res => {
       debugger;
       console.log(res)
     });
+  }
+
+
+  addToRequsetList(reqItemLine){
+    // this.itemLine.value;
+    // this.itemNumInput;
+    reqItemLine;
+    debugger;
+    if(reqItemLine.itemNumInput!=""&&reqItemLine.itemAmount!=""){
+      let reqListItem={
+        itemNumber:reqItemLine.itemNumInput,
+        itemName:"TBD",
+        amount:reqItemLine.itemAmount,
+        relatedOrder:reqItemLine.relatedOrder,
+      }
+      this.reqList.push(reqListItem);
+      debugger;
+    }
+    
+  }
+
+  deleteRow(itemNum,itemAmout){
+    this.reqList.forEach(function(item, index, object){
+      if (item.itemNumber==itemNum && item.amount==itemAmout ) {
+        object.splice(index, 1)
+      }
+    });
+
   }
 }
