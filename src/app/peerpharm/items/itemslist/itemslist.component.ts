b16938aa69b314c93f0c4890443ee033de17d821
USER <gabi@gmail.com>
2019-01-09 16:00:19 +0200

    license limitation

--- src/app/peerpharm/items/itemslist/itemslist.component.ts
+++ src/app/peerpharm/items/itemslist/itemslist.component.ts
@@ -56,4 +56,43 @@ export class ItemslistComponent implements OnInit {
       this.items= this.items.filter(x=>x.itemFullName.toLowerCase().includes(word.toLowerCase())); 
     }
   }
+
+  updateLicenseQuotaLimitation(myevent, itemNumber, licsensNumber,licenceExprationDate, licenceFileLink,licenceLastUpdateDate, licenceLastUpdateUser, licenceNotifaction){
+    console.log(myevent);
+    let QuotaVal;
+    let hasLimition;
+    if(licenceFileLink==undefined) licenceFileLink="";
+    if(licenceLastUpdateUser==undefined) licenceLastUpdateUser="";
+    if(licenceLastUpdateDate==undefined) licenceLastUpdateDate="";
+    if(licenceNotifaction==undefined) licenceNotifaction="";
+
+    if(myevent.target.type=="checkbox"){
+      QuotaVal= myevent.target.nextSibling.value;
+      hasLimition= myevent.target.checked;
+
+    } else if (myevent.target.type=="number"){
+     hasLimition= myevent.target.previousElementSibling.checked
+     QuotaVal= myevent.target.value;
+
+debugger
+    }
+    
+    let docObj = {
+      // licsensNumber: licsensNumber,
+      // licenceExprationDate: licenceExprationDate,
+      // licenceFileLink: licenceFileLink,
+      // licenceLastUpdateDate: licenceLastUpdateDate,
+      // licenceLastUpdateUser: licenceLastUpdateUser,
+      // licenceNotifaction: licenceNotifaction,
+      itemNumber:itemNumber,
+      licenceItemsQuotaLimit: QuotaVal,
+      licenceHasItemsQuotaLimition: hasLimition,
+    }
+    
+  this.itemsService.updateLicenseLimition(docObj).subscribe(res => console.log("updateLicenseLimition: "+res));
+  }
+
+
+
+
 }
