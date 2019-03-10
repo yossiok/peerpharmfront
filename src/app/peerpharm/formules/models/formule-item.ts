b4b084d9aaac78f8a8ea3b8f928c39c8cf6de32f
USER <gabi@gmail.com>
2019-03-10 10:33:54 +0200

    formule changes

--- src/app/peerpharm/formules/models/formule-item.ts
+++ src/app/peerpharm/formules/models/formule-item.ts
@@ -3,11 +3,8 @@ export class FormuleItem {
   number: number;//מספר פריט חו"ג במלאי -פנימי שלנו 
   name: string;
   quantity: number;
+  quantityUnits:String;
   percentage: number;
   itemPH: number;
   instructions: string;
-  suplierNumber: string; //מספר מזהה של הספק -ממערכת קומקס
-  suplierName: string;// שם הספק של החו"ג-ממערכת קומקס
-  suplierItemNumber: string; // מק"ט של הפריט אצל הספק -ממערכת קומקס 
-  procurementItemNumber: string;// מספר פריט רכש - מערכת קומקס
 }
