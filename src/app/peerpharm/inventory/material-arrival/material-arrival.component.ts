aad14d5da57995eab436cd4a655d06fc8630a329
USER <gabi@gmail.com>
2019-05-02 16:22:22 +0300

    material-arrival -not done

--- src/app/peerpharm/inventory/material-arrival/material-arrival.component.ts
+++ src/app/peerpharm/inventory/material-arrival/material-arrival.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { InventoryService } from 'src/app/services/inventory.service';
+import { Procurementservice } from 'src/app/services/procurement.service';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-material-arrival',
@@ -8,7 +13,30 @@ import { Component, OnInit } from '@angular/core';
 export class MaterialArrivalComponent implements OnInit {
 
   today: Date = new Date();
-  constructor() { }
+  newMaterialArrival: FormGroup;
+  constructor(private fb: FormBuilder, private invtSer:InventoryService, private procuretServ: Procurementservice, private toastSrv: ToastrService, 
+    private authService: AuthService,) {
+      
+    this.newMaterialArrival = fb.group({
+      deliveryDate: [Date, new Date(), Validators.required], 
+      productionDate: [Date, null], 
+      internalNumber: ["", Validators.required],
+      materialName: ["", Validators.required], 
+      lotNumber: ["", Validators.required], 
+      supplierName: ["", Validators.required], 
+      supplierNumber: ["", Validators.required], 
+      analysisApproval: [Boolean,false, Validators.required], 
+      totalQnt: [0, Validators.required], 
+      packageType: ["", Validators.required], //select 
+      packageQnt: [0, Validators.required],              
+    
+      remarks: ["", ],
+      lastUpdateDate: [Date, Validators.nullValidator],
+      lastUpdateUser: ["", Validators.nullValidator],
+      status: ["", Validators.nullValidator],
+    });
+
+   }
 
   ngOnInit() {
   }
