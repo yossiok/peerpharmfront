528c53da8e78c57389de72610ca0220465cac01a
Bar <barlevinn@gmail.com>
2019-12-18 14:33:47 +0200

    twoFactor

--- src/app/peerpharm/peerpharm.routing.ts
+++ src/app/peerpharm/peerpharm.routing.ts
@@ -247,7 +247,7 @@ export const PeerPharmRputs: Routes =[
       title: 'Formule Table'
     },
     component: AllFormulesComponent,
-    // canActivate:[TwoFactor]
+    canActivate:[TwoFactor]
   },
   {
     path :'production/lines',
