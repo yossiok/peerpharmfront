3d672f618abf802c541888424a433d462e0392f1
Bar <barlevinn@gmail.com>
2020-01-30 09:55:30 +0200

    changes

--- src/app/peerpharm/batches/batches-mkp/batches-mkp.component.ts
+++ src/app/peerpharm/batches/batches-mkp/batches-mkp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { BatchesService } from 'src/app/services/batches.service';
 import { ItemsService } from 'src/app/services/items.service';
 import { ToastrService } from 'ngx-toastr';
@@ -12,6 +12,17 @@ export class BatchesMkpComponent implements OnInit {
 
 
   mkpBatches:any[];
+  currentItem:any;
+  currentItemName:any;
+  currentBarrels:any;
+  currentBatchNumber:any;
+  currentExpDate:any;
+  currentProduced:any;
+  currentOrderN:any;
+  currentPH:any;
+  currentWeightKG:any;
+
+  @ViewChild('printBtn') printBtn: ElementRef;
 
 
   newMkpBatch = {
@@ -42,12 +53,45 @@ export class BatchesMkpComponent implements OnInit {
   }
 
   addNewMkpBatch(){
+  debugger;
+  this.currentItem = this.newMkpBatch.item
+  this.currentItemName = this.newMkpBatch.itemName
+  this.currentBarrels = this.newMkpBatch.barrels
+  this.currentBatchNumber = this.newMkpBatch.batchNumber
+  this.currentExpDate = this.newMkpBatch.expration
+  this.currentProduced = this.newMkpBatch.produced
+  this.currentOrderN = this.newMkpBatch.order
+  this.currentPH = this.newMkpBatch.ph
+  this.currentWeightKG =this.newMkpBatch.weightKg
+
+  if(this.newMkpBatch.batchNumber != "") {
     this.batchService.addNewMkpBatch(this.newMkpBatch).subscribe(data=>{
       this.mkpBatches = data;
+      setTimeout(() => {
+        this.printBtn.nativeElement.click();          
+      }, 500);
+
       this.toastSr.success("נוספה אצווה חדשה")
+     this.newMkpBatch.item = ""
+   this.newMkpBatch.itemName = ""
+      this.newMkpBatch.barrels = ""
+  this.newMkpBatch.batchNumber = ""
+      this.newMkpBatch.expration = ""
+    
+     this.newMkpBatch.order =""
+      this.newMkpBatch.ph= ""
+     this.newMkpBatch.weightKg =""
 
     })
   }
+
+  
+  
+  }
+
+
+
+  
 
 
   fillItemName(ev){
