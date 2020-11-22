059fc77ad3a1f21879b3f4f596157fac9b1256f2
Bar <barlevinn@gmail.com>
2020-11-22 09:06:29 +0200

    changes

--- src/app/peerpharm/peerpharm.routing.ts
+++ src/app/peerpharm/peerpharm.routing.ts
@@ -65,7 +65,7 @@ import { ProjectsComponent } from './schedule/projects/projects.component';
 import { ActiveusersComponent } from './reports/activeusers/activeusers.component';
 import { ReportBuilderComponent } from './reports/report-builder/report-builder.component';
 import { UserSettingsComponent } from './user/user-settings/user-settings.component';
-import { CalendarComponent } from './calendar/calendar.component';
+
 
 
 
