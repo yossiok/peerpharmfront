7d2ee01a00cadece6867b5f2453cce646c5608ac
Bar <barlevinn@gmail.com>
2021-03-01 14:09:50 +0200

    inventory search bug + reload app every ? hours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    
+    var now = new Date();
+    var then = new Date();
+    var dayUTC = new Date();
+
+    if(dayUTC.getUTCDay() == 1) {
+
+        if(now.getUTCHours() > 24 ||
+       (now.getUTCHours() == 24 && now.getUTCMinutes() > 0) ||
+        now.getUTCHours() == 24 && now.getUTCMinutes() == 0 && now.getUTCSeconds() >= 0) {
+        then.setUTCDate(now.getUTCDate() + 1);
+        }
+
+    then.setUTCHours(24);
+    then.setUTCMinutes(0);
+    then.setUTCSeconds(0);
+
+
+    var timeout = (then.getTime() - now.getTime());
+    setTimeout(function() { window.location.reload(); }, timeout);
+    }
+  }
+
+
 }
