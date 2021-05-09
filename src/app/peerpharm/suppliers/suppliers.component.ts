836f107e2564946eb4f9c9b02848816d5057a84e
Bar <barlevinn@gmail.com>
2021-05-09 12:31:23 +0300

    suppliers phase 1

--- src/app/peerpharm/suppliers/suppliers.component.ts
+++ src/app/peerpharm/suppliers/suppliers.component.ts
@@ -8,6 +8,24 @@ import { ExcelService } from 'src/app/services/excel.service';
 import { ActivatedRoute } from '@angular/router';
 import { InventoryService } from 'src/app/services/inventory.service';
 
+const defaultSupplier = {
+  suplierNumber: '',
+  suplierName: '',
+  address: '',
+  city: '',
+  phoneNum: '',
+  cellularNum: '',
+  faxNum: '',
+  lastUpdated: '',
+  country: '',
+  email: '',
+  contactName: '',
+  currency: '',
+  remarks: '',
+  alternativeSupplier: [],
+  priceList: [],
+  import: ''
+}
 
 @Component({
   selector: 'app-suppliers',
@@ -18,216 +36,125 @@ import { InventoryService } from 'src/app/services/inventory.service';
 
 
 export class SuppliersComponent implements OnInit {
-  closeResult: string;
-  suppliersAlterArray:any[];
-  suppliers: any[];
-  itemPurchases: any[];
-  suppliersCopy: any[];
-  alternSupplier: any[];
-  alterSupplierToPush:string;
-  alterSupplierArray:any[] = [];
-  tableType:string = "suppliers";
-  suppliersOrderItems:any[];
-  suppliersOrderItemsCopy:any[];
-  hasMoreItemsToload: boolean = true;
-  updateSupplier: boolean = false;
-  showItemPurchases: boolean = false;
-  currentSupplier = {
-    suplierNumber: '',
-    suplierName: '',
-    address: '',
-    city: '',
-    phoneNum: '',
-    cellularNum: '',
-    faxNum: '',
-    lastUpdated: '',
-    country:'',
-    email:'',
-    contactName:'',
-    currency:'',
-    remarks:'',
-    alternativeSupplier:this.alterSupplierArray,
-    items:[],
-    priceList:[],
-    import:''
-  }
-
-  priceListItem = {
-    itemNumber:'',
-    itemName:'',
-    supplierPrice:'',
-    itemCoin:''
-  }
-
-  supplier = {
-    suplierNumber: '',
-    suplierName: '',
-    address: '',
-    city: '',
-    phoneNum: '',
-    cellularNum: '',
-    faxNum: '',
-    lastUpdated: '',
-    country:'',
-    email:'',
-    contactName:'',
-    currency:'',
-    remarks:'',
-    alternativeSupplier:this.alterSupplierArray,
-    items:[],
-    import:''
-
-  }
-
-
   @ViewChild('fromDateStr') fromDateStr: ElementRef;
   @ViewChild('toDateStr') toDateStr: ElementRef;
-  private container: ElementRef;
+  addEditText: string;
   @ViewChild('container') set content(content: ElementRef) {
     this.container = this.content;
   }
 
+  closeResult: string;
+  suppliersAlterArray: any[];
+  suppliers: any[];
+  itemPurchases: any[];
+  suppliersCopy: any[];
+  alternSupplier: any[];
+  alterSupplierToPush: string;
+  alterSupplierArray: any[] = [];
+  tableType: string = "suppliers";
+  suppliersOrderItems: any[];
+  suppliersOrderItemsCopy: any[];
+  hasMoreItemsToload: boolean = true;
+  supplierModal: boolean = false;
+  showItemPurchases: boolean = false;
+  currentSupplier: any = defaultSupplier
+  supItems: any[] = []
+  supPurchases: any[] = []
+  private container: ElementRef;
+  showPurchaseItems: boolean;
+  purchaseStockitems: any[] = []
+  purchaseNumber: any;
 
+  constructor(private inventoryService: InventoryService, private route: ActivatedRoute, private excelService: ExcelService, private procurementService: Procurementservice, private modalService: NgbModal, private supplierService: SuppliersService, private renderer: Renderer2, private toastSrv: ToastrService) { }
 
-  constructor(private inventoryService:InventoryService,private route: ActivatedRoute,private excelService: ExcelService,private procurementService: Procurementservice, private modalService: NgbModal, private supplierService: SuppliersService, private renderer: Renderer2, private toastSrv: ToastrService) { }
-
-  open(content) {
-    
-    this.supplier = {
-      suplierNumber: '',
-      suplierName: '',
-      address: '',
-      city: '',
-      phoneNum: '',
-      cellularNum: '',
-      faxNum: '',
-      lastUpdated: '',
-      country:'',
-      email:'',
-      contactName:'',
-      currency:'',
-      remarks:'',
-      alternativeSupplier:this.alterSupplierArray,
-      items:[],
-      import:''
-
-    }
-
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      
-      console.log(result);
-
-      if (result == 'Saved') {
-        this.saveSupplier();
-      }
-      this.closeResult = `Closed with: ${result}`;
-      console.log(this.closeResult);
-    }, (reason) => {
-
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+  ngOnInit() {
+    this.getSuppliers();
+    this.getAlternativeSuppliers();
+    // this.getSuppliersOrderedItems();
   }
 
-  openDetails(content, i) {
- 
-    console.log(this.suppliers[i]);
-    this.supplier = this.supplier[i];
-    this.modalService.open(content).result.then((result) => {
-      console.log(result);
-      if (result == 'Saved') {
-        this.saveSupplier();
-      }
-    })
-  }
 
-  fillItemName(ev){
-    ;
-  var itemNumber = ev.target.value;
-  
-  if(itemNumber != ''){
-    this.inventoryService.getCmptByitemNumber(itemNumber).subscribe(data=>{
-      if(data){
-        this.priceListItem.itemName = data[0].componentName
-      }
-    })
-  }
-  }
 
   getAlternativeSuppliers() {
-   
     this.supplierService.getAllAlternativeSuppliers().subscribe(res => {
       this.alternSupplier = res
       console.log(this.alternSupplier);
-
-
     });
   }
 
   getSuppliers() {
-    
     this.supplierService.getAllSuppliers().subscribe(res => {
       this.suppliers = res
       this.suppliersCopy = res
-      
       var currentAlterSupp = [];
       this.suppliers.forEach(function (supplier) {
         currentAlterSupp.push(supplier.alternativeSupplier);
       });
       this.suppliersAlterArray = currentAlterSupp;
-});
-
-}
-
-getSuppliersOrderedItems() {
- 
-  this.procurementService.getProcurementOrderItem().subscribe(res => {
-    this.suppliersOrderItems = res
-    this.suppliersOrderItemsCopy = res
-
-    if(res.length == res.length) {
-      this.hasMoreItemsToload == false;
-    }
-    
-    console.log(this.suppliersOrderItems)
-});
-
-}
-
-setType(type) {
-
-  switch (type) {
-    case 'suppliers':
-      this.tableType = "suppliers"
-      break;
-    case 'supplierReports':
-    this.tableType = "supplierReports"
-      break;
+    });
 
   }
-}
 
+  // getSuppliersOrderedItems() {
+  //   this.procurementService.getProcurementOrderItem().subscribe(res => {
+  //     this.suppliersOrderItems = res
+  //     this.suppliersOrderItemsCopy = res
+  //     if(res.length == res.length) {
+  //       this.hasMoreItemsToload == false;
+  //     }
+  //     console.log(this.suppliersOrderItems)
+  //   });
+  // }
 
-  saveSupplier() {
-    ;
-    if(this.supplier.suplierName != "" && this.supplier.suplierNumber != "") {
-    this.supplierService.addorUpdateSupplier(this.supplier).subscribe(res => {
-      console.log(res);
-      // if (res == "updated") this.toastSrv.info(this.supplier.suplierName, "Changes Saved");
-      // else if (res.includes("Saved")) {
-      
-        this.toastSrv.success(this.supplier.suplierName, "New Costumer Saved");
-        this.suppliers.push(this.supplier);
-        
-      
-      // else this.toastSrv.error("Failed" , res);
+  getAllPurchasesFromSup() {
+    this.procurementService.getAllOrdersFromSupplier(this.currentSupplier.suplierNumber).subscribe(data => {
+      this.supPurchases = data.filter(purchase => purchase.status == 'open')
+      for (let order of data) {
+        for (let item of order.stockitems) {
+          this.supItems.push({
+            orderNumber: order.orderNumber,
+            arrivalDate: order.arrivalDate,
+            ...item
+          })
+        }
+      }
+      console.log(data)
+      console.log('items: ', this.supItems)
     })
-  } else { 
-    this.toastSrv.error("Please fill all the missing fields");
   }
+
+  openItems(stockitems, orderNumber) {
+    this.showPurchaseItems = true
+    this.purchaseStockitems = [...stockitems]
+    this.purchaseNumber = orderNumber
   }
+
+  filterSuppliers(by, e) {
+    this.suppliers = this.suppliersCopy.filter(supplier => supplier[by] && supplier[by].includes(e.target.value))
+  }
+
+  filterSuppliersByItem(itemNumber) {
+
+  }
+
+  filterSuppliersByOpenOrders() {
+
+  }
+
+  setType(type) {
+    switch (type) {
+      case 'suppliers':
+        this.tableType = "suppliers"
+        break;
+      case 'supplierReports':
+        this.tableType = "supplierReports"
+        break;
+    }
+  }
+
+
 
   private getDismissReason(reason: any): string {
-  
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
@@ -237,205 +164,73 @@ setType(type) {
     }
   }
 
-  changeText(ev)
-  {
-    
-    let word= ev.target.value;
-    let wordsArr= word.split(" ");
-    wordsArr= wordsArr.filter(x=>x!="");
-    if(wordsArr.length>0){
-      
-      let tempArr=[];
-      this.suppliers.filter(x=>{
-        
-        var check=false;
-        var matchAllArr=0;
-        wordsArr.forEach(w => {
-         
-            if(x.suplierName.toLowerCase().includes(w.toLowerCase()) ){
-              matchAllArr++
-            }
-            (matchAllArr==wordsArr.length)? check=true : check=false ; 
-        }); 
-
-        if(!tempArr.includes(x) && check) tempArr.push(x);
-      });
-         this.suppliers= tempArr;
-         this.hasMoreItemsToload = false;
-         
-    }else{
-      
-      this.suppliers=this.suppliersCopy.slice();
-    }
-  }
 
 
-  searchSupplier(supplierName){
-;
-    var word = supplierName
-    if(word != "" && word != undefined){
-      this.suppliers = this.suppliersCopy.filter(s=>s.suplierName == supplierName);
-      this.openData(this.suppliers[0].suplierNumber)
-    }
-    else {
-      this.suppliers = this.suppliers
-    }
-    
 
-  }
-
-  searchName(ev)
-  {
-  
-      var word = ev.target.value;
-
-    var wordsArr= word.split(" ");
-    wordsArr= wordsArr.filter(x=>x!="");
-    if(wordsArr.length>0){
-      
-      let tempArr=[];
-      this.suppliersOrderItems.filter(x=>{
-        
-        var check=false;
-        var matchAllArr=0;
-        wordsArr.forEach(w => {
-         
-            if(x.supplierName.toLowerCase().includes(w.toLowerCase()) ){
-              matchAllArr++
-            }
-            (matchAllArr==wordsArr.length)? check=true : check=false ; 
-        }); 
-
-        if(!tempArr.includes(x) && check) tempArr.push(x);
-      });
-         this.suppliersOrderItems= tempArr;
-         this.hasMoreItemsToload = false;
-         
-    }else{
-      
-      this.suppliersOrderItems=this.suppliersOrderItemsCopy.slice();
-    }
-  }
-
-  searchNumber(ev)
-  {
-    
-    let word= ev.target.value;
-    
-    let wordsArr= word.split(" ");
-    wordsArr= wordsArr.filter(x=>x!="");
-    if(wordsArr.length>0){
-      
-      let tempArr=[];
-      this.suppliersOrderItems.filter(x=>{
-        
-        var check=false;
-        var matchAllArr=0;
-        wordsArr.forEach(w => {
-         
-            if(x.itemNumber==w ){
-              matchAllArr++
-            }
-            (matchAllArr==wordsArr.length)? check=true : check=false ; 
-        }); 
-
-        if(!tempArr.includes(x) && check) tempArr.push(x);
-      });
-         this.suppliersOrderItems= tempArr;
-         this.hasMoreItemsToload = false;
-    }else{
-      
-      this.suppliersOrderItems=this.suppliersOrderItemsCopy.slice();
-    }
-  }
-
-  addItemToPriceList(){
-    ;
-    if(this.priceListItem.itemName != '' && this.priceListItem.itemNumber != '' && this.priceListItem.supplierPrice != '' ){
-      this.currentSupplier.priceList.push(this.priceListItem)
-      this.priceListItem = {
-        itemName:'',
-        itemNumber:'',
-        supplierPrice:'',
-        itemCoin:'',
-      }
-      this.toastSrv.success('פריט נוסף בהצלחה - לא לשכוח לעדכן !')
-    } else {
-      this.toastSrv.error('אנא תמלא את כל הפרטים')
-    }
-
-  }
-
-  dateChange(){
-    
-    if (this.fromDateStr.nativeElement.value != "" && this.toDateStr.nativeElement.value != "" ) {
-
-      this.procurementService.getProcurementOrderItemByDate(this.fromDateStr.nativeElement.value, this.toDateStr.nativeElement.value).subscribe(data=>{
+  dateChange() {
+    if (this.fromDateStr.nativeElement.value != "" && this.toDateStr.nativeElement.value != "") {
+      this.procurementService.getProcurementOrderItemByDate(this.fromDateStr.nativeElement.value, this.toDateStr.nativeElement.value).subscribe(data => {
         this.suppliersOrderItems = data;
         this.suppliersOrderItemsCopy = data;
       })
     }
-  
   }
 
-  addAlterSupplier() { 
-    
+  addAlterSupplier() {
     let alterSuppToPush = this.alterSupplierToPush
     this.alterSupplierArray.push(alterSuppToPush)
     this.toastSrv.success("Alternative supplier added")
-    
   }
 
-  openData(supplierNumber){
-  ;
-    this.supplierService.getSuppliersByNumber(supplierNumber).subscribe(data=>{
-      if(data){
-        this.currentSupplier = data[0]
-        this.updateSupplier = true
-      }     
+  openData(addEdit: string, index?) {
+    this.addEditText = addEdit
+    if(index) {
+      this.currentSupplier = this.suppliers[index]
+      this.getAllPurchasesFromSup()
+    }
+    this.supplierModal = true
+  }
+
+  showAllPurchases(itemNumber) {
+    this.procurementService.getAllItemPurchases(itemNumber).subscribe(data => {
+      if (data) {
+        this.itemPurchases = data;
+        this.showItemPurchases = true;
+      }
     })
   }
 
-  showAllPurchases(itemNumber){
-    ;
-    this.procurementService.getAllItemPurchases(itemNumber).subscribe(data=>{
-    if(data){
-     this.itemPurchases = data; 
-     this.showItemPurchases = true;
-
+  updateCurrSupplier() {
+    if(this.addEditText == 'Update Supplier') {
+      this.supplierService.updateCurrSupplier(this.currentSupplier).subscribe(data => {
+        if (data) {
+          this.toastSrv.success('ספק עודכן בהצלחה !');
+          this.supplierModal = false;
+          this.getSuppliers();
+        }
+      })
     }
-    })
-  }
-
-  updateCurrSupplier(){
-    this.supplierService.updateCurrSupplier(this.currentSupplier).subscribe(data=>{
-  ;
-    if(data){
-      this.toastSrv.success('ספק עודכן בהצלחה !');
-      this.updateSupplier = false;
-      this.getSuppliers();
+    else if(this.addEditText == 'Add Supplier'){
+      this.supplierService.addorUpdateSupplier(this.currentSupplier).subscribe(res => {
+        if(res.msg == 'Supplier Number Allready Exist') this.toastSrv.error(res.msg)
+        else this.toastSrv.success(res.msg)
+      })
     }
-    })
   }
 
 
 
-  exportAsXLSX():void {
-    this.excelService.exportAsExcelFile(this.suppliersOrderItems, 'sample');
-  }
 
-  ngOnInit() {
-    
-    this.getSuppliers();
-    this.getAlternativeSuppliers();
-    this.getSuppliersOrderedItems();
-    ;
-    if(this.route.queryParams){
-      setTimeout(() => {
-        this.searchSupplier(this.route.snapshot.queryParams.supplierName)
-      }, 1000);
-      
+
+  exportAsXLSX(data, fileName) {
+    if (fileName == fileName) {
+      data.forEach(object => {
+        delete object.__v
+        delete object._id
+      })
     }
-    
+    this.excelService.exportAsExcelFile(data, fileName);
   }
+
+
 }
