f6c4857400f3bf8e302454004a7fc93ab8f053c4
Bar <barlevinn@gmail.com>
2021-06-06 10:35:54 +0300

    inventommy1

--- src/app/peerpharm/inventory/wharehouse/wharehouse.component.ts
+++ src/app/peerpharm/inventory/wharehouse/wharehouse.component.ts
@@ -527,11 +527,10 @@ debugger
   async sendList() {
 
     let ObjToUpdate;
-    ;
+    debugger
     let sendConfirm = confirm("עדכון שינויים במלאי");
     if (sendConfirm && this.inventoryUpdateList.length > 0) {
-debugger
-      await this.inventoryService.updateInventoryChangesTest(this.inventoryUpdateList, this.inventoryUpdateList[0].itemType).subscribe(async res => {
+      await this.inventoryService.updateInventoryChangesTest(this.inventoryUpdateList, this.inventoryUpdateList[0].itemType, this.dir).subscribe(async res => {
         // res = [itemNumber,itemNumber,itemNumber...]
         if (res == "all updated" || (res.msg && res.msg == "all updated")) {
           if (res.reception) this.certificateReception = res.reception
