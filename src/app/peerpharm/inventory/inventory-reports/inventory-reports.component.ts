d9cd5e05c968eff0bf2d1eb77b01c143365516a2
Bar <barlevinn@gmail.com>
2021-08-23 17:36:15 +0300

    galia's mail + tomer's mail

--- src/app/peerpharm/inventory/inventory-reports/inventory-reports.component.ts
+++ src/app/peerpharm/inventory/inventory-reports/inventory-reports.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ExcelService } from 'src/app/services/excel.service';
 import { InventoryService } from 'src/app/services/inventory.service';
+import { Procurementservice } from 'src/app/services/procurement.service';
 
 @Component({
   selector: 'app-inventory-reports',
@@ -12,6 +13,7 @@ export class InventoryReportsComponent implements OnInit {
 
   loader: boolean = false
   cmptTypes: Array<any>
+  purchaseOrders: Array<any>
 
   reportForm = new FormGroup({
     cmptType: new FormControl(''),
@@ -20,16 +22,17 @@ export class InventoryReportsComponent implements OnInit {
 
   constructor(
     private inventorySer: InventoryService,
-    private excelService: ExcelService
-  ) { 
-   }
+    private excelService: ExcelService,
+    private purchaseService: Procurementservice
+  ) {
+  }
 
   ngOnInit(): void {
     this.getTypes()
   }
 
   getTypes() {
-    this.inventorySer.getAllComponentTypes().subscribe(allTypes=>{
+    this.inventorySer.getAllComponentTypes().subscribe(allTypes => {
       this.cmptTypes = allTypes
     })
   }
@@ -39,41 +42,50 @@ export class InventoryReportsComponent implements OnInit {
     this.inventorySer.getInvRep(this.reportForm.value).subscribe(data => {
       this.loader = false
       if (this.reportForm.value.itemType == 'product') {
-        data.map( item => {
+        data.map(item => {
           item.name ? item.name = `${item.name[0]} ${item.subName[0]} ${item.description[0]}` : null
           delete item.subName
           delete item.description
         })
-          
       }
       else {
-        data.map(item=>{
+        data.map(item => {
           delete item.shell_id_in_whareHouse
           delete item.deliveryNoteNum
-          if (this.reportForm.value.itemType != 'component') {
+          delete item._id
+          delete item.whareHouseID
+          if (this.reportForm.value.itemType != 'material') {
             delete item.expirationDate
             delete item.productionDate
           }
+
+          // get purchase details for components and materials
+          if (this.reportForm.value.itemType != 'product') {
+            item.name = item.stockitem[0].componentName
+            item.openOrders = ''
+            item.orderedAmount = 0
+            for (let order of item.purchases) {
+              if (order.status != 'closed' && order.status != 'canceled') {
+                item.openOrders += ',' + order.orderNumber // purchase order numbers
+                let i = order.stockitems.findIndex(i => i.name == item.name)
+                item.orderedAmount += Number(order.stockitems[i].quantity) // purchase amounts
+              }
+            }
+          }
           delete item.batchNumber
           delete item.supplierBatchNumber
-          if(this.reportForm.value.itemType != 'all') delete item.itemType
+          if (this.reportForm.value.itemType != 'all') delete item.itemType
           delete item.__v
           delete item.barcode
-          delete item.stockitem
           delete item.userName
-          delete item.shell_id_in_whareHouse
-          delete item.shell_id_in_whareHouse
-          delete item.shell_id_in_whareHouse
-          delete item.shell_id_in_whareHouse
-          delete item.shell_id_in_whareHouse
-          delete item.shell_id_in_whareHouse
           delete item.shell_id_in_whareHouse
           return item
         })
       }
-        // item.subName ? item.subName = item.subName[0] : null
-        // item.description ? item.description = item.description[0] : null
-      this.excelService.exportAsExcelFile(data, 'Inventory Report')
+      // item.subName ? item.subName = item.subName[0] : null
+      // item.description ? item.description = item.description[0] : null
+      let sortOrder = ['item', 'name', 'position', 'whareHouse', 'amount', 'orderedAmount', 'openOrders']
+      this.excelService.exportAsExcelFile(data, 'Inventory Report', sortOrder)
     })
   }
 
