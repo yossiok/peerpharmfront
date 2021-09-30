b58087be211a8d32670b407dd2ce9bd05f9eae45
Bar <barlevinn@gmail.com>
2021-09-30 10:42:11 +0300

    fix materials

--- src/app/peerpharm/inventory/wharehouse/hist-movements/hist-movements.component.ts
+++ src/app/peerpharm/inventory/wharehouse/hist-movements/hist-movements.component.ts
@@ -3,8 +3,10 @@ import {
   ElementRef,
   EventEmitter,
   Input,
+  OnChanges,
   OnInit,
   Output,
+  SimpleChanges,
   ViewChild,
 } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
@@ -20,16 +22,16 @@ import { WarehouseService } from "src/app/services/warehouse.service";
   templateUrl: "./hist-movements.component.html",
   styleUrls: ["./hist-movements.component.scss"],
 })
-export class HistMovementsComponent implements OnInit {
+export class HistMovementsComponent implements OnInit, OnChanges {
   @Input() allWhareHouses: any[];
   @Output() initTabByName = new EventEmitter<any>();
 
   movementTypes = [
     { value: "in", name: "כניסה" },
     { value: "out", name: "יציאה" },
-    // { value: "whareHouseChange", name: "העברה בין מחסנים" },
-    // { value: "shelfChange", name: "העברה בין מדפים" },
-    // { value: "production", name: "העברה לייצור" },
+    { value: "whareHouseChange", name: "העברה בין מחסנים" },
+    { value: "shelfChange", name: "העברה בין מדפים" },
+    { value: "production", name: "העברה לייצור" },
   ];
   allUsers: any = [];
   queryObj: object = {};
@@ -52,22 +54,29 @@ export class HistMovementsComponent implements OnInit {
     private purchaseService: Procurementservice,
     private userService: UsersService,
     private warehouseService: WarehouseService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getAllUsers();
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.allWhareHouses = changes.allWhareHouses.currentValue
+  }
+
   getAllUsers() {
     this.userService.getAllUsers().subscribe(
       (users) =>
-        (this.allUsers = users.sort((a, b) => {
-          if (a.userName.toLowerCase() > b.userName.toLowerCase()) return 1;
-          else return -1;
-        }))
+      (this.allUsers = users.sort((a, b) => {
+        if (a.userName.toLowerCase() > b.userName.toLowerCase()) return 1;
+        else return -1;
+      }))
     );
   }
 
   getReceptions() {
+
+    // build the query
     let formValues = this.historicalMovements.controls;
     this.histMovements = [];
     let queryString = "?";
@@ -77,23 +86,28 @@ export class HistMovementsComponent implements OnInit {
         queryString += `${item}=${formValues[item].value}&`;
       }
     }
+
     queryString = queryString.slice(0, -1);
     this.inventoryService.getHistMovements(queryString).subscribe((data) => {
       data.forEach((element) => {
-        let type;
-        let wh;
-        type = this.movementTypes.find((obj) => {
-          return obj.value == element.movementType;
-        });
-        element.movementName = type.name;
-        element.logs.forEach((log) => {
-          wh = this.allWhareHouses.find((obj) => {
-            return obj._id == log.WH_originId;
+        if (!element.itemType) {
+
+          let type;
+          let wh;
+
+          type = this.movementTypes.find((obj) => {
+            return obj.value == element.movementType;
           });
-          log.whareHouse = wh.name;
-        });
+          element.movementName = type ? type.name : 'NA';
+          element.logs.forEach((log) => {
+            wh = this.allWhareHouses.find((obj) => {
+              return obj._id == log.WH_originId
+            });
+            log.whareHouse = wh.name;
+          });
+        }
       });
-      this.histMovements = data;
+      this.histMovements = data.filter(e => !e.itemType);
     });
   }
 
