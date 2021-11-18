47b00903359266d5fa4234d9fc574b99624fa752
Dani Morag <dmorag90@gmail.com>
2021-11-18 08:17:18 +0200

    Add new component for order items and workplans

--- src/app/pipes/work-plan-status.pipe.ts
+++ src/app/pipes/work-plan-status.pipe.ts
@@ -1,21 +1,36 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'workPlanStatus'
+  name: "workPlanStatus",
 })
 export class WorkPlanStatusPipe implements PipeTransform {
-
   transform(value: number): string {
-    let status = ''
-    switch(value) {
-      case 1: status = 'Draft'
-      break
-      case 2: status = 'Moved to Production'
-      break
-      case 3: status = 'in Production'
-      break
+    let status = "";
+    switch (value) {
+      case 1:
+        status = "Draft";
+        break;
+      case 2:
+        status = "Moved to Production";
+        break;
+      case 3:
+        status = "in Production";
+        break;
+      case 4:
+        status = "Manufactured";
+        break;
+      case 5:
+        status = "Closed";
+        break;
+      case 6:
+        status = "On Hold";
+        break;
+      case 7:
+        status = "Cancelled";
+        break;
+      default:
+        status = "Unknown";
     }
-    return status
+    return status;
   }
-
 }
