01c3d30c36f7d217ca1eb96d2f68da28431e602c
Dani Morag <dmorag90@gmail.com>
2021-12-06 08:59:43 +0200

    YearCount shelf list component excel file upload.

--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -1,24 +1,42 @@
-import { Injectable } from '@angular/core';
-import * as FileSaver from 'file-saver';
-import * as XLSX from 'xlsx';
-const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-const EXCEL_EXTENSION = '.xlsx';
+import { Injectable } from "@angular/core";
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
+const EXCEL_TYPE =
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+const EXCEL_EXTENSION = ".xlsx";
 @Injectable()
 export class ExcelService {
-constructor() { }
-public exportAsExcelFile(json: any[], excelFileName: string, order?: any[]): void {
-  const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json, {header: order});
-  const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-  const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-  if(order) {
-    const range = XLSX.utils.decode_range(worksheet['!ref']);
-    range.e['c'] = order.length - 1;
-    worksheet['!ref'] = XLSX.utils.encode_range(range);
+  constructor() {}
+  public exportAsExcelFile(
+    json: any[],
+    excelFileName: string,
+    order?: any[],
+    sheetName?: string
+  ): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json, {
+      header: order,
+    });
+    let sName = sheetName ? sheetName : "data";
+    const workbook: XLSX.WorkBook = {
+      Sheets: { [sName]: worksheet },
+      SheetNames: [sName],
+    };
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    if (order) {
+      const range = XLSX.utils.decode_range(worksheet["!ref"]);
+      range.e["c"] = order.length - 1;
+      worksheet["!ref"] = XLSX.utils.encode_range(range);
+    }
+    this.saveAsExcelFile(excelBuffer, excelFileName);
   }
-  this.saveAsExcelFile(excelBuffer, excelFileName);
-}
-private saveAsExcelFile(buffer: any, fileName: string): void {
-   const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
-   FileSaver.saveAs(data, fileName + '_export_' + new  Date().getTime() + EXCEL_EXTENSION);
-}
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(
+      data,
+      fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+    );
+  }
 }
