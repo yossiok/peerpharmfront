b0c60e999e739e52a7414ba8dfaeef33a4f78e2e
Erangr90 <erangrady3.0@gmail.com>
2022-04-20 14:04:58 +0300

    qa logs finish

--- src/app/peerpharm/qa/qalogs/qaLogs.component.ts
+++ src/app/peerpharm/qa/qalogs/qaLogs.component.ts
@@ -15,7 +15,20 @@ export class QaLogsComponent implements OnInit {
   isLogin:boolean = false;
   table:Array<any>;
   pageNumber:number = 1;
-  showLogsModal:boolean = true;
+  showLogsModal:boolean = false;
+
+  lineSort:number =1;
+  batchSort:number =1;
+  openSort:number =1;
+  closeSort:number =1;
+  workTimeSort:number =1;
+  qtySort:number =1;
+  produceSort:number =1;
+  PercentageSort:number =1;
+  unitSort:number =1;
+
+  logsArray:Array<any> = [];
+  
 
   constructor(
     private logService:LogsService,
@@ -84,7 +97,136 @@ export class QaLogsComponent implements OnInit {
     this.getUserInfo();
     this.logService.getQaLogs(String(this.pageNumber)).subscribe((res)=>{
       this.table = res.table;
-      console.log(this.table);
     });    
   }
+
+  sortByLine(){
+    if(this.lineSort == 1){
+      this.lineSort = -1;
+      this.table.sort((a,b)=>{
+        return (Number(a.line) - Number(b.line))
+      })
+      return
+
+    }else{
+      this.lineSort = 1;
+      this.table.sort((a,b)=>{
+        return (Number(b.line) - Number(a.line))
+      })
+      return
+
+    }
+  }
+
+  sortByBatch(){
+    if(this.batchSort == 1){
+      this.batchSort = -1;
+      this.table.sort((a,b)=>{
+        return (Number(a.batch.slice(0,2)) - Number(b.batch.slice(0,2)) || Number(a.batch.slice(4)) - Number(b.batch.slice(4)))
+      })
+      return
+
+    }else{
+      this.batchSort = 1;
+      this.table.sort((a,b)=>{
+        return (Number(b.batch.slice(0,2)) - Number(a.batch.slice(0,2)) || Number(b.batch.slice(4)) - Number(a.batch.slice(4)))
+      })
+      return
+
+    }
+  }
+
+
+  sortByCloseLot(){
+    if(this.closeSort == 1){
+      this.closeSort = -1;
+      this.table.sort((a,b)=>{
+
+        return (
+          Number(a.closeLot.split(",")[0].split("-")[0]) - Number(b.closeLot.split(",")[0].split("-")[0])
+          ||
+          Number(a.closeLot.split(",")[0].split("-")[1]) - Number(b.closeLot.split(",")[0].split("-")[1])
+          ||
+          Number(a.closeLot.split(",")[0].split("-")[2]) - Number(b.closeLot.split(",")[0].split("-")[2])
+          ||
+          Number(a.closeLot.split(",")[1].trim().split(":")[0]) - Number(b.closeLot.split(",")[1].trim().split(":")[0])
+          ||
+          Number(a.closeLot.split(",")[1].trim().split(":")[1]) - Number(b.closeLot.split(",")[1].trim().split(":")[1])
+          )
+      })
+      return
+
+    }else{
+      this.closeSort = 1;
+      this.table.sort((a,b)=>{
+
+
+        return (
+          Number(b.closeLot.split(",")[0].split("-")[0]) - Number(a.closeLot.split(",")[0].split("-")[0])
+          ||
+          Number(b.closeLot.split(",")[0].split("-")[1]) - Number(a.closeLot.split(",")[0].split("-")[1])
+          ||
+          Number(b.closeLot.split(",")[0].split("-")[2]) - Number(a.closeLot.split(",")[0].split("-")[2])
+          ||
+          Number(b.closeLot.split(",")[1].trim().split(":")[0]) - Number(a.closeLot.split(",")[1].trim().split(":")[0])
+          ||
+          Number(b.closeLot.split(",")[1].trim().split(":")[1]) - Number(a.closeLot.split(",")[1].trim().split(":")[1])
+        )
+      })
+      return
+
+    }
+  }
+  sortByOpenLot(){
+    if(this.openSort == 1){
+      this.openSort = -1;
+      this.table.sort((a,b)=>{
+
+        return (
+          Number(a.openLot.split(",")[0].split("/")[2]) - Number(b.openLot.split(",")[0].split("/")[2])
+          ||
+          Number(a.openLot.split(",")[0].split("/")[1]) - Number(b.openLot.split(",")[0].split("/")[1])
+          ||
+          Number(a.openLot.split(",")[0].split("/")[0]) - Number(b.openLot.split(",")[0].split("/")[0])
+          ||
+          Number(a.openLot.split(",")[1].trim().split(":")[0]) - Number(b.openLot.split(",")[1].trim().split(":")[0])
+          ||
+          Number(a.openLot.split(",")[1].trim().split(":")[1]) - Number(b.openLot.split(",")[1].trim().split(":")[1])
+          )
+      })
+      return
+
+    }else{
+      this.openSort = 1;
+      this.table.sort((a,b)=>{
+
+
+        return (
+          Number(b.openLot.split(",")[0].split("/")[2]) - Number(a.openLot.split(",")[0].split("/")[2])
+          ||
+          Number(b.openLot.split(",")[0].split("/")[1]) - Number(a.openLot.split(",")[0].split("/")[1])
+          ||
+          Number(b.openLot.split(",")[0].split("/")[0]) - Number(a.openLot.split(",")[0].split("/")[0])
+          ||
+          Number(b.openLot.split(",")[1].trim().split(":")[0]) - Number(a.openLot.split(",")[1].trim().split(":")[0])
+          ||
+          Number(b.openLot.split(",")[1].trim().split(":")[1]) - Number(a.openLot.split(",")[1].trim().split(":")[1])
+        )
+      })
+      return
+
+    }
+  }
+
+  updateLogsArray(row){
+    if(this.logsArray.includes(row)){
+      this.logsArray = this.logsArray.filter((x)=> x.line != row.line && x.batch != row.batch && x.openLot != row.openLot && x.closeLot != row.closeLot)
+    }else{
+      this.logsArray.push(row)
+    }
+    console.log(this.logsArray.length);
+  }
+ 
+
+
 }
